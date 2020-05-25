c890e8a255d073465f85d7ff44da66b7acc20760
Mohamadreza <mohamadrezafathibesharat@gmail.com>
2020-05-25 13:50:46 +0430

    Add project files.

---
+++ DigitalShoppingTakkala/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DigitalShoppingTakkala.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
