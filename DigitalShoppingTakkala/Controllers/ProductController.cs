7bc6986bbdda856c384e8861bd0e2fc806bb1e09
Mohamadreza <mohamadrezafathibesharat@gmail.com>
2020-06-04 20:20:40 +0430

    create filtering part -section1

--- DigitalShoppingTakkala/Controllers/ProductController.cs
+++ DigitalShoppingTakkala/Controllers/ProductController.cs
@@ -16,15 +16,43 @@ namespace DigitalShoppingTakkala.Controllers
         {
             _ctx = ctx;
         }
-        public IActionResult GetAllBySGID(int id)
+        public IActionResult GetAllBySGID(int id, string sorting)
         {
-           
-            return View(_ctx.Products.Where(p=>p.SubGroupId == id).ToList());
+            var products = _ctx.Products.Where(p => p.SubGroupId == id).ToList();
+            products = products.OrderBy(x => x.Price2).ToList();
+
+            if (!string.IsNullOrEmpty(sorting))
+            {
+                if (sorting == "1")
+                {
+                    products = products.OrderBy(x => x.status).ToList();
+                }
+                if (sorting == "2")
+                {
+                    products = products.OrderByDescending(x => x.status).ToList();
+                }
+
+                if (sorting == "5")
+                {
+                    products = products.OrderBy(x => x.Price2).ToList();
+                }
+                if (sorting == "4")
+                {
+                    products = products.OrderByDescending(x => x.Price2).ToList();
+                }
+            }
+
+            else
+            {
+                products = products.OrderBy(x => x.ProductId).ToList();
+            }
+
+            return View(products);
         }
 
         public IActionResult GetProductByid(int id)
         {
-            
+
             return View(_ctx.Products.Find(id));
         }
     }
