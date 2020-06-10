0a3880c4ba03ddf42302e665fba1ee916ea78c72
Mohamadreza <mohamadrezafathibesharat@gmail.com>
2020-06-11 02:57:02 +0430

    add  show order(bsket) with editing buttons

--- DigitalShoppingTakkala/Controllers/OrderController.cs
+++ DigitalShoppingTakkala/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using DigitalShoppingTakkala.Data;
 using DigitalShoppingTakkala.Models;
+using DigitalShoppingTakkala.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -68,7 +69,7 @@ namespace DigitalShoppingTakkala.Controllers
                     details.Count += 1;
                     _ctx.Update(details);
                 }
-               
+
                 _ctx.SaveChanges();
             }
             UpdateSumOrder(order.OrderId);
@@ -83,7 +84,64 @@ namespace DigitalShoppingTakkala.Controllers
         }
         public IActionResult ShowOrder()
         {
-            return View();
+            string CurrentUserID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            Order order = _ctx.Orders.SingleOrDefault(o => o.UserId == CurrentUserID && !o.IsFinally);
+            List<ShowOrderViewModel> _list = new List<ShowOrderViewModel>();
+            if (order != null)
+            {
+                var details = _ctx.OrderDetails.Where(d => d.OrderId == order.OrderId).ToList();
+                foreach (var item in details)
+                {
+                    var product = _ctx.Products.Find(item.ProductId);
+                    _list.Add(new ShowOrderViewModel()
+                    {
+                        Count = item.Count,
+                        ImageName = product.ImageName,
+                        OrderDetailId = item.OrderDetailId,
+                        ProductName = product.ProductName,
+                        Price = item.FinalPrice,
+                        Sum = item.Count * item.FinalPrice
+
+                    });
+
+                }
+            }
+
+            return View(_list);
+
         }
+        public IActionResult Delete(int id)
+        {
+            var orderdetail = _ctx.OrderDetails.Find(id);
+            _ctx.Remove(orderdetail);
+            _ctx.SaveChanges();
+            return RedirectToAction("ShowOrder");
+        }
+        public IActionResult Editing(int id,int ud)
+        {
+            var orderdetail = _ctx.OrderDetails.Find(id);
+            if (ud == 1)
+            {
+                orderdetail.Count += 1;
+                _ctx.Update(orderdetail);
+            }
+            else if(ud==2)
+            {
+                orderdetail.Count -= 1;
+                if (orderdetail.Count == 0)
+                {
+                    _ctx.OrderDetails.Remove(orderdetail);
+                   
+                }
+                else { _ctx.Update(orderdetail); }
+                
+            }
+            
+            _ctx.SaveChanges();
+            return RedirectToAction("ShowOrder");
+        }
+
+
     }
 }
---
+++ DigitalShoppingTakkala/Models/ViewModels/ShowOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalShoppingTakkala.Models.ViewModels
+{
+    public class ShowOrderViewModel
+    {
+        public int OrderDetailId { get; set; }
+        public string ProductName { get; set; }
+        public string ImageName { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+        public double Sum { get; set; }
+
+    }
+}
